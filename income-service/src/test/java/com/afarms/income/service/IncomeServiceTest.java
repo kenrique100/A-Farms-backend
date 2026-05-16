7c17624225f5f13a5cc1c017a1c0174e9eb71be8
Kenrique100 <ngwakenri2016@gmail.com>
2026-05-17 00:53:36 +0100

    fix income test

--- income-service/src/test/java/com/afarms/income/service/IncomeServiceTest.java
+++ income-service/src/test/java/com/afarms/income/service/IncomeServiceTest.java
@@ -1,14 +1,16 @@
 package com.afarms.income.service;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import com.afarms.income.client.TransactionServiceClient;
 import com.afarms.income.client.UserServiceClient;
 import com.afarms.income.repository.IncomeRepository;
 import com.afarms.income.service.impl.IncomeServiceImpl;
+import com.afarms.income.utils.IncomeBuilderUtils;
 import com.afarms.income.utils.IncomeValidationUtils;
+import com.afarms.income.utils.ResponseBuilderUtils;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class IncomeServiceTest {
 
@@ -17,13 +19,17 @@ class IncomeServiceTest {
         IncomeRepository repository = Mockito.mock(IncomeRepository.class);
         UserServiceClient userServiceClient = Mockito.mock(UserServiceClient.class);
         TransactionServiceClient transactionServiceClient = Mockito.mock(TransactionServiceClient.class);
-        IncomeValidationUtils incomeValidationUtils = Mockito.mock(IncomeValidationUtils.class);
+        IncomeValidationUtils validationUtils = Mockito.mock(IncomeValidationUtils.class);
+        IncomeBuilderUtils builderUtils = Mockito.mock(IncomeBuilderUtils.class);
+        ResponseBuilderUtils responseBuilder = Mockito.mock(ResponseBuilderUtils.class);
 
         IncomeServiceImpl service = new IncomeServiceImpl(
                 repository,
                 userServiceClient,
                 transactionServiceClient,
-                incomeValidationUtils
+                validationUtils,
+                builderUtils,
+                responseBuilder
         );
         assertNotNull(service);
     }
